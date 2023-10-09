e9963679937135bd817c3e3ceb551a6fa1efaa61
duncan <duncan.kampert@surf.nl>
2023-10-09 12:24:04 +0200

    Initial commit

---
+++ games/__init__.py
@@ -0,0 +1,6 @@
+import games.connect4.connect4
+import games.connect4.connect4_nn
+import games.hive.hive
+import games.hive.hive_nn
+import games.tictactoe.tictactoe
+import games.tictactoe.tictactoe_nn
---
+++ tests/test_simulator.py
@@ -0,0 +1,35 @@
+import unittest
+
+from games.tictactoe.tictactoe import TicTacToe
+from games.tictactoe.tictactoe_nn import TicTacToeNN
+from agz.simulator import Simulator
+
+
+class MyTestCase(unittest.TestCase):
+    game = TicTacToe
+    nn = TicTacToeNN
+
+    def test_simulator(self):
+
+        p1 = self.nn(self.game.input_space, self.game.action_space)
+        p2 = self.nn(self.game.input_space, self.game.action_space)
+
+        for i in range(1):
+            simulator = Simulator(self.game, mcts_iterations=25)
+            data = simulator.parallel_play(1, p1, p2)
+
+            boards, policys, result, perspective = data
+
+            for board, policy in zip(boards, policys):
+                print(board[0] + board[1] * 2)
+                print(policy)
+                print(result)
+                print("\n\n")
+
+            # if result == GameState.DRAW_REPETITION:
+            #     for node in game.history:
+            #         node.print()
+
+
+if __name__ == '__main__':
+    unittest.main()
